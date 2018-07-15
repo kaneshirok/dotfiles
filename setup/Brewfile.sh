89b24e917d185a8092d0e8ec7bd88b40ea052578
k.kaneshiro <k.kaneshiro@churadata.okinawa>
2018-07-15 15:05:49 +0900

    [modify]brewfileを自分用に修正

--- setup/Brewfile.sh
+++ setup/Brewfile.sh
@@ -3,25 +3,17 @@
 brew update
 brew upgrade
 
-brew install zplug
-brew install neovim
 brew install wget
 brew install tree
 brew install rmtrash
-brew install oysttyer
 brew install peco
 brew install jq
 brew install awscli
 brew install ghq
 
-brew tap sanemat/font
-brew install ricty
-cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ${HOME}/Library/Fonts/
-fc-cache -vf
-
 brew tap caskroom/cask
 
-brew cask install hyper
+brew cask install visual-studio-code
 brew cask install vivaldi
 brew cask install docker
 brew cask install java
@@ -32,15 +24,15 @@ brew cask install postico
 brew cask install sequel-pro
 brew cask install appcleaner
 brew cask install insomnia
+brwe cask install postman
+brew cask install sourcetree
+brew cask install iterm2
+brew cask install skype
 
 brew install mas
 
-mas install 409183694   # Keynote
 mas install 443987910   # 1Password
 mas install 803453959   # Slack
 mas install 485812721   # TweetDeck
-mas install 407963104   # Pixelmator
 mas install 1091189122  # Bear
-mas install 1024640650  # CotEditor
 mas install 568494494   # Pocket
-mas install 411680127   # WiFi Scanner
