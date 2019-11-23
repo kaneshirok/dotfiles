9e3ab1f8440cea82cb11411e9cf6f6d311770375
Kazuki Kaneshiro <ktmlike3214@gmail.com>
2019-11-23 21:54:40 +0900

    Add files via upload

--- setup/Brewfile.sh
+++ setup/Brewfile.sh
@@ -23,9 +23,9 @@ brew cask install google-chrome
 brew cask install google-japanese-ime
 brew cask install cmd-eikana
 brew cask install postico
+brew cask install dbeaver-community
 brew cask install sequel-pro
 brew cask install appcleaner
-brew cask install insomnia
 brwe cask install postman
 brew cask install sourcetree
 brew cask install iterm2
@@ -34,18 +34,18 @@ brew cask install chatwork
 brew cask install pycharm-ce
 brew cask install netspot
 brew cask install kindle
-brew cask install unity
-brew cask install virtualbox
+brew cask install unity-hub
+brew cask install slack
+brew cask install table-tool
+brew caks install the-unarchiver
+brew caks install zoomus
 
 brew install mas
 
 mas install 443987910   # 1Password
-mas install 803453959   # Slack
-mas install 485812721   # TweetDeck
 mas install 1091189122  # Bear
 mas install 568494494   # Pocket
 mas install 1167193104  # CSView
-mas install 1122008420  # Table Tool
-mas install 425424353   # The Unarchiver
-mas install 907364780   # Tomato One
 mas install 1278508951  # Trello
+mas install 715768417   # Microsoft Remote Desktop (8.0.30030)
+mas install 1142151959  # Just Focus
